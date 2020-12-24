b0c5316a1a15023b3540929adfc9591c142ff7b3
casualprops <fridrik92@gmail.com>
2020-12-24 16:43:06 +0000

    Changed to a single page react bootstrap app instead.

---
+++ ClientApp/src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+);
